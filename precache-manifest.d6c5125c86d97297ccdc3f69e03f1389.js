1856cb5f64642d061185d63050be45e0fcb89bc5
Nimish Srivastava <nimish2025@gmail.com>
2021-05-22 14:07:46 +0530

    Updates

---
+++ precache-manifest.d6c5125c86d97297ccdc3f69e03f1389.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba450ebd511c140d3604e32de51b3cfd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9fd4ab324269c5054ea1",
+    "url": "./static/css/main.d4e98635.chunk.css"
+  },
+  {
+    "revision": "71bf32be4b49e19b8227",
+    "url": "./static/js/2.5de25323.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.5de25323.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fd4ab324269c5054ea1",
+    "url": "./static/js/main.1d001b14.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.1d001b14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2c4b718b5c6409a3d15",
+    "url": "./static/js/runtime-main.8b929383.js"
+  }
+]);
